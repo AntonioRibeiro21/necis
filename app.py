c2d8cc806678f5fa6250f0c014c6d58ff48d4154
AntonioRibeiro21 <antonio.ribeiro@devio.com.br>
2022-02-14 15:27:16 -0300

    deploy

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import numpy as np
 from datetime import date
 from flask_cors import CORS
 
-cors = CORS(app, resources={r"/*": {"origins": "https://usp-municipios.vercel.app"}})
+cors = CORS(app, resources={r"/*": {"origins": "https://usp-municipios.vercel.app/"}})
 
 # from fbprophet.diagnostics import cross_validation, performance_metrics
 # import itertools
@@ -201,156 +201,84 @@ def ReturnData():
 
     return data
 
-# @app.route('/Teste', methods=['GET','POST'])
-# def Teste():
-#     today = date.today()
-#     select_mun = request.form.get("Municipios", None)
-#     select_dp = request.form.get("Delegacias", None)
-#     ano_fim = int(request.form.get("Ano", None))
-#     ano_ini = ano_fim
-#     forecast_yrs = ano_fim - today.year + 1
+@app.route('/geral', methods=['GET','POST'])
+def Teste():
+    today = date.today()
+    select_mun = request.form.get("Municipios", None)
+    select_dp = request.form.get("Delegacias", None)
+    ano_fim = int(request.form.get("Ano", None))
+    ano_ini = ano_fim
+    forecast_yrs = ano_fim - today.year + 1
     
-#     crimes = [
-#     "Homicídio Doloso", 
-#     "Homicídio Doloso por acidente de trânsito", 
-#     "Homicídio Culposo por acidente de trânsito", 
-#     "Homicídio Culposo - Outros", 
-#     "Tentativa de Homicídio", 
-#     "Lesão Corporal seguida de morte",
-#     "Lesão Corporal Dolosa",
-#     "Lesão Corporal Culposa por acidente de trânsito",
-#     "Lesão Corporal Culposa - Outras",
-#     "Latrocínio",
-#     "Estupro",
-#     "Roubo - Outros",
-#     "Roubo de veículo",
-#     "Roubo a banco",
-#     "Roubo de carga",
-#     "Furto - Outros",
-#     "Furto de veículo"
-#     ], 
+    crimes = [
+    "Homicídio Doloso", 
+    "Homicídio Doloso por acidente de trânsito", 
+    "Homicídio Culposo por acidente de trânsito", 
+    "Homicídio Culposo - Outros", 
+    "Tentativa de Homicídio", 
+    "Lesão Corporal seguida de morte",
+    "Lesão Corporal Dolosa",
+    "Lesão Corporal Culposa por acidente de trânsito",
+    "Lesão Corporal Culposa - Outras",
+    "Latrocínio",
+    "Estupro",
+    "Roubo - Outros",
+    "Roubo de veículo",
+    "Roubo a banco",
+    "Roubo de carga",
+    "Furto - Outros",
+    "Furto de veículo"
+    ], 
            
-#     result = np.zeros(17)
-#     result = result.tolist()
-#     df = pd.DataFrame()
-#     list = []
-#     for i, aux in enumerate(crimes[0]):
-#         df = getDataAtDB(select_mun, select_dp, crimes[0][i])
-#         df['datas'] = pd.to_datetime(df['datas'])
-#         df.set_index('datas')
-#         df.columns = ["ds", "y"]
-#         m = Prophet(
-#         changepoint_prior_scale=0.05,
-#         changepoint_range=0.8
-#             )
-#         m.add_country_holidays(country_name='BR')
-#         m.fit(df)
-#         future = m.make_future_dataframe(periods=12*forecast_yrs, freq='MS')
-#         forecast = m.predict(future)
-#         print(forecast)
-        # forecast['trend'] = forecast['trend'].apply(np.floor)
-        # forecast.resample('AS', on='ds').sum()
-
-        # years = int(ano_fim)+1 - int(ano_ini)
-
-        # output = np.zeros(years)
-        # output = output.tolist()
-
-        # teste = forecast['trend']
-
-        # teste = teste.to_numpy()
-        # teste = np.flip(teste)
-        # teste = teste.tolist()
-        # initial_len = len(teste) -1
-        # if len(teste) < len(output) : 
-        #     for idx, val in enumerate(output):
-        #         if idx <= initial_len:
-        #             None
-        #         else: 
-        #             teste.append('0')
-        #     output = teste
-        # else:
-        #     for idx, val in enumerate(output):
-        #         output[idx] = teste[idx]
-
-        # output.reverse()
-        # print(output)
-    #     list.append(output)
-    # return {"data": list}    
-        #print(forecast)
-    #return {}
+    result = np.zeros(17)
+    result = result.tolist()
+    df = pd.DataFrame()
+    list = []
+    for i, aux in enumerate(crimes[0]):
+        df = getDataAtDB(select_mun, select_dp, crimes[0][i])
+        df['datas'] = pd.to_datetime(df['datas'])
+        df.set_index('datas')
+        df.columns = ["ds", "y"]
         
-    #     df['datas'] = pd.to_datetime(df['datas'])
-    #     #df['ano'] = df['datas'].dt.year
-    #     df.set_index('datas')
-    #     df.columns = ["ds", "y"]
-    #     return df.to_json(orient='records')
-    #     m = Prophet(
-    #             changepoint_prior_scale=0.05,
-    #             changepoint_range=0.8
-    #         )
-    #     m.add_country_holidays(country_name='BR')
-    #     m.fit(df)
+        m = Prophet(
+        changepoint_prior_scale=0.05,
+        changepoint_range=0.8
+            )
+        m.add_country_holidays(country_name='BR')
+        m.fit(df)
+        future = m.make_future_dataframe(periods=12*forecast_yrs, freq='MS')
+        forecast = m.predict(future)
         
-    #     future = m.make_future_dataframe(periods=12*int(select_ano), freq='MS')
-    #     forecast = m.predict(future)
-    #     forecast['trend'] = forecast['trend'].apply(np.floor)
-    #     forecast.resample('AS', on='ds').sum()
+        forecast['trend'] = forecast['trend'].apply(np.floor)
+        forecast.resample('AS', on='ds').sum()
 
-    #     years = int(ano_fim)+1 - int(ano_ini)
+        years = int(ano_fim)+1 - int(ano_ini)
 
-    #     output = np.zeros(years)
-    #     output = output.tolist()
+        output = np.zeros(years)
+        output = output.tolist()
 
-    #     teste = forecast['trend']
+        teste = forecast['trend']
 
-    #     teste = teste.to_numpy()
-    #     teste = np.flip(teste)
-    #     teste = teste.tolist()
-    #     initial_len = len(teste) -1
-    #     if len(teste) < len(output) : 
-    #         for idx, val in enumerate(output):
-    #             if idx <= initial_len:
-    #                 None
-    #             else: 
-    #                 teste.append('0')
-    #         output = teste
-    #     else:
-    #         for idx, val in enumerate(output):
-    #             output[idx] = teste[idx]
+        teste = teste.to_numpy()
+        teste = np.flip(teste)
+        teste = teste.tolist()
+        initial_len = len(teste) -1
+        if len(teste) < len(output) : 
+            for idx, val in enumerate(output):
+                if idx <= initial_len:
+                    None
+                else: 
+                    teste.append('0')
+            output = teste
+        else:
+            for idx, val in enumerate(output):
+                output[idx] = teste[idx]
 
-    #     output.reverse()
-    #     print(output)
-    #     result.append(output[0])
-
-    # return {"data": result}
+        output.reverse()
         
-
-    # if(int(select_ano) > 2021):
-
-    #     forecast_yrs = select_ano - today.year + 1
-    #     df = getDataAno(select_dp, select_ano)
-    #     df['datas'] = pd.to_datetime(df['datas'])
-    #     df.columns = ["type","ds", "y"]
-    #     df_by_type = df.groupby('type')
-    #     return df.to_json(orient='records')
-    #     # for index, row in df.iterrows():
-    #     #     print(row['c1'], row['c2'])
-    #     #     df.set_index('datas')
-    #     #     df.columns = ["type","ds", "y"]
-    #     #     m = Prophet(
-    #     #         changepoint_prior_scale=0.05,
-    #     #         changepoint_range=0.8
-    #     #     )
-    #     #     m.add_country_holidays(country_name='BR')
-    #     #     m.fit(df)
-    #     #     future = m.make_future_dataframe(periods=12*int(forecast_yrs), freq='MS')
-    #     #     forecast = m.predict(future)
-        
-    #     # return forecast.to_json(orient='records')
-    # else:
-    #     df = getDataAno(select_dp, select_ano)
-    #     return df.to_json(orient='records')
+        list.append(output)
+    return {"data": list}    
+    
 
 if __name__ == "__main__":
     app.run()
